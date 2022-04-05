48785bd2c52b0b0455019a9a847f1b82e46e1965
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-05 16:54:33 +0530

    [I'm Repo] Add: My New Programm

---
+++ NewProgram2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+namespace Helloworld
+{
+    class Helloworld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, i am Sunny Sejwar");
+        }
+    }
+}
